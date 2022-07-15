8bab30930250a4c479a793dbfa847ff973b7da54
abhinay <abhnay.yadav@gmail.com>
2022-07-15 08:33:19 +0530

    django first commit

---
+++ test_app/serializers.py
@@ -0,0 +1,9 @@
+# from django.contrib import admin
+from rest_framework import serializers
+from .models import Student
+
+
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student
+        fields = ['id', 'stuname', 'email']
